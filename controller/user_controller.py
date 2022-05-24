643fa54eb162159b4680a4539cc6702683725e1b
gohan <jmarin94171@umanizales.edu.co>
2022-05-23 22:16:07 -0500

    add code

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,13 +1,30 @@
 from flask import Response,json,jsonify,Blueprint, request
-from service.user_service import User
+from service.user_service import UserService
 from util.util_encoder import UtilEncoder
+from flask_jwt_extended import create_access_token, jwt_required,get_jwt_identity
 
 app_user = Blueprint("app_user",__name__)
 
-user_service = User()
+user_service = UserService()
 
 @app_user.route('/user')
-def Create():
-
-    return Response(status=200, response=json.dumps(user_service.Create_user(),
+def list():
+    return Response(status=200, response=json.dumps(user_service.read_users(),
                                                     cls=UtilEncoder), mimetype="application/json")
+
+@app_user.route('/user/login', methops=['POST'])
+def login():
+    try:
+        email = request.json.get('email')
+        password = request.json.get('password')
+        user = user_service.login(email, password)
+        access_token = create_access_token(identity={'user': user})
+        return jsonify({'token': access_token})
+    except Exception as e:
+        return jsonify({'message': str(e)})
+
+#terminar el token
+#revisar el codigo del profe para hacer el dto
+#hacer todo en usuario service en un solo controller
+#hacer metodo posicionar(definir) barco, validar disparo
+
--- service/user_service.py
+++ service/user_service.py
@@ -1,37 +1,84 @@
 from model.user import User
 from model.type_user import TypeUser
-
+from model.user_dto import UserDTO
 
 class UserService:
+
     def __init__(self):
         self.admin = None
         self.player_1 = None
         self.player_2 = None
 
     def create_user(self, data):
-        count = 0
+        count_player = 0
+        count_admin = 1
         type_user = data['type_user']
 
-        if type_user == 1:
+        if type_user == 1 and count_admin == 1:
             self.admin = User(data, 1, TypeUser(1, "Administrador"))
+            count_admin += 1
 
-        if type_user == 2:
+        elif type_user == 1 and count_admin > 1:
+            raise Exception("Ya hay un administrador registrado")
+
+        if type_user == 2 and count_player == 0:
             self.player_1 = User(data, 2, TypeUser(2, "Jugador"))
-            count =+1
+            count_player += 1
 
-        if type_user == 2 and count == 1:
+        if type_user == 2 and count_player == 1:
             self.player_2 = User(data, 3, TypeUser(2, "Jugador"))
+            count_player += 1
+
+        elif type_user == 2 and count_player > 2:
+            raise Exception("Ya hay dos jugadores registrados")
 
         else:
             raise Exception("tipo de usuario invalido")
 
+    def validate_email(self, data):
+        list = self.read_users()
+        email = data['email']
+        for user in list:
+            if user.email == email:
+                raise Exception('Este email ya esta siendo utilizado')
 
-    def Read_user(self):
+
+    def login(self):
         pass
 
-    def update_user(self):
-        pass
+    def read_users(self,):
+        list = []
 
-    def delete_user(self):
+        if self.admin != None:
+            list.append(self.admin.toDTO)
 
-        pass
+        if self.player_1 != None:
+            list.append(self.player_1.toDTO)
+
+        if self.player_2 != None:
+            list.append(self.player_2.toDTO)
+
+        return list
+
+
+    def delete_user(self, data):
+
+        email= data["email"]
+        if email == self.admin.email:
+            raise Exception("No es posible eliminar el administrador")
+
+        elif email == self.player_1.email:
+            self.player_1 = None
+            self.count_player -= 1
+
+        elif email == self.player_2.email:
+            self.player_2 = None
+            self.count_player -= 1
+
+        else:
+            raise Exception("No se ha encontrado ningun usuario")
+
+
+
+
+
